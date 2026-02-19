8135dcdb9056ff6d703e52c34d53ee3179c6b1d6
seasherls06 <salinas.sherly06@gmail.com>
2026-02-19 15:41:01 +0800

    PIT

--- js/script.js
+++ js/script.js
@@ -1,27 +1,34 @@
-console.log("js/script.js")
+console.log("js/script.js");
 
 const submitBtn = document.getElementById("submitBtn");
-
-submitBtn.addEventListener("click",function(event){
+submitBtn.addEventListener("click", function(event) {
 	event.preventDefault();
 	const name = document.getElementById("name").value;
 	const email = document.getElementById("email").value;
-
-	if (name===""||email===""){
-	   alert("Please fill in all required fields.");
-        } else{
-	alert("Form submitted successfully!");
+	if (name === "" || email === "") {
+		alert("Please fill in all required fields.");
+	} else {
+		alert("Form submitted successfully!");
 	}
 });
 
 const themeToggle = document.getElementById("themeToggle");
-	themeToggle.addEventListener("click",function(){
+themeToggle.addEventListener("click", function() {
 	document.body.classList.toggle("dark-mode");
+	if (document.body.classList.contains("dark-mode")) {
+		themeToggle.textContent = "Light Mode";
+	} else {
+		themeToggle.textContent = "Dark Mode";
+	}
 });
 
 const toggleSkills = document.getElementById("toggleSkills");
 const skillsSection = document.getElementById("skillsSection");
-
-toggleSkills.addEventListener("click",function(){
+toggleSkills.addEventListener("click", function() {
 	skillsSection.classList.toggle("hidden");
+	if (skillsSection.classList.contains("hidden")) {
+		toggleSkills.textContent = "Show Skills";
+	} else {
+		toggleSkills.textContent = "Hide Skills";
+	}
 });
